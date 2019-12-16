9c3fe53c25c64c96de8095111c304d2af798a688
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-12-16 11:21:52 -0600

    refactor studios container

--- client/src/components/StudiosContainer.js
+++ client/src/components/StudiosContainer.js
@@ -1,93 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class StudiosContainer extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-          studios: {}
-        }
-        this.createStudioCard = this.createStudioCard.bind(this) //not sure if this helps
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      studios: []
+    };
+  }
 
   async getStudios() {
     try {
-      const response = await fetch('http://localhost:3000/api/v1/studios');
+      const response = await fetch("http://localhost:3001/api/v1/studios");
       const data = await response.json();
-      this.setState({studios: data});
+      console.log(data);
+      this.setState({ studios: data });
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   }
 
-  createStudioCard(studioDetail){ //can't seem to figure out how to eaily add a lot of html
-    const div = document.createElement('div className="studioCard"')
-    const name = document.createElement('h1')
-    const class_type = document.createElement('h3')
-    const description = document.createElement('p')
-    const address = document.createElement('p')
-    
-    name.innerHTML = studioDetail.name
-    class_type.innerHTML = studioDetail.class_type
-    description.innerHTML = studioDetail.description
-    address.innerHTML = studioDetail.address
-
-    div.appendChild(name)
-    div.appendChild(class_type)
-    div.appendChild(description)
-    div.appendChild(address)
-
-    console.log(div) //can't get here
-  }
-
-  createStudiosList() {
-   const ul = document.getElementById('studioList')
-   this.state.studios.map(function(studio){
-    let li = document.createElement('li')
-    console.log(this) // this is undefined
-    //const studioCard = <createStudioCard studioDetail={studio} /> //this function is not called correctly. I also tried this.createStudioCard(studio) and studio.createStudioCard(), etc.
-    //li.innerHTML = studioCard //studio.name wroks, but want to absctract
-    const div = document.createElement('div')
-    div.setAttribute("class", "studioCard")
-    const name = document.createElement('h1')
-    const class_type = document.createElement('h3')
-    const description = document.createElement('p')
-    const address = document.createElement('p')
-    
-    name.innerHTML = studio.name
-    class_type.innerHTML = studio.class_type
-    description.innerHTML = studio.description
-    address.innerHTML = studio.address
-
-    div.appendChild(name)
-    div.appendChild(class_type)
-    div.appendChild(description)
-    div.appendChild(address)
-    li.appendChild(div)
-    ul.appendChild(li)
-   })
+  createStudioCards(studios) {
+    return studios.map(studio => {
+      return (
+        <li>
+          <div className="studioCard">
+            <h1>{studio.name}</h1>
+            <h3>{studio.class_type}</h3>
+            <p>{studio.description}</p>
+            <p>{studio.address}</p>
+          </div>
+        </li>
+      );
+    });
   }
 
   async componentDidMount() {
     await this.getStudios();
-    this.createStudiosList()
   }
-  
-  
-    render() {
+
+  render() {
+    console.log(this.state.studios);
     return (
       <div>
-	<div className="inputContainer">
-	  <input className="taskInput" type="text" 
-	    placeholder="search" maxLength="50" />
-	</div>  	    
-	<div className="listWrapper">
-	   <ul id="studioList" className="taskList"> 
-	   </ul>
-	</div>
-      </div>    
-    )
+        <div className="inputContainer">
+          <input
+            className="taskInput"
+            type="text"
+            placeholder="search"
+            maxLength="50"
+          />
+        </div>
+        <div className="listWrapper">
+          <ul id="studioList">{this.createStudioCards(this.state.studios)}</ul>
+        </div>
+      </div>
+    );
   }
 }
 
-export default StudiosContainer
+export default StudiosContainer;
