6343bb6f1c259b8b081234470d836b60e69c1dbd
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-12-17 07:07:29 -0600

    added classes index view

--- client/src/Containers/ClassesContainer.js
+++ client/src/Containers/ClassesContainer.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import ClassCards from "../components/ClassCards";
+import APICalls from "../components/APICalls";
 
 class ClassesContainer extends Component {
   constructor(props) {
@@ -6,6 +8,48 @@ class ClassesContainer extends Component {
     this.state = {
       classes: []
     };
+  }
+
+  async getClasses() {
+    try {
+      const response = await fetch(
+        "http://localhost:3001/api/v1/studio_classes"
+      );
+      const data = await response.json();
+      console.log(data);
+      this.setState({ classes: data });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async componentDidMount() {
+    await this.getClasses();
+    // {
+    //   this.getStudios();
+    // }
+    // <APICalls />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="listWrapper">
+          <div className="inputContainer">
+            <input
+              className="classInput"
+              type="text"
+              placeholder="search"
+              maxLength="50"
+            />
+          </div>
+          <h3>Classes:</h3>
+          <ul id="classList">
+            <ClassCards classes={this.state.classes} />
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ client/src/components/ClassCards.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+const ClassCards = ({ classes }) => {
+  return classes.map(card => {
+    return (
+      <li key={card.id.toString()}>
+        <div className="classCard">
+          <h1>{card.name}</h1>
+          <h3>Type: {card.class_type}</h3>
+          <p>{card.description}</p>
+          <p>
+            <strong>Occupancy: </strong>
+            {card.occupancy}
+          </p>
+        </div>
+      </li>
+    );
+  });
+};
+
+export default ClassCards;
